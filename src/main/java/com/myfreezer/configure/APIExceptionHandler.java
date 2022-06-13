d24150df656e8c8fc4e2efd9c09656ea35d0ed90
Ahmed Marzook <ahmed.marzook@cgi.com>
2022-06-13 22:46:42 +0100

    Updated ApiExceptionhandler

--- src/main/java/com/myfreezer/configure/APIExceptionHandler.java
+++ src/main/java/com/myfreezer/configure/APIExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -16,12 +17,14 @@ import java.util.Map;
 public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoDataFoundException.class)
-    public ResponseEntity<Object> handleNodataFoundException(
-            NoDataFoundException ex, WebRequest request) {
+    public ResponseEntity<Object> handleNodataFoundException(NoDataFoundException ex, HttpServletRequest request) {
 
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("error", HttpStatus.NOT_FOUND);
         body.put("message", ex.getMessage());
+        body.put("path", request.getRequestURL());
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
